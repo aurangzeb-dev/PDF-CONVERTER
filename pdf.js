f6d033e286f3f9ee148f30d7c66365b4f3283fee
aurangzeb <aurangzebmohd15@gmail.com>
2024-08-26 00:39:59 +0530

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -15,27 +15,34 @@ imageInput.addEventListener('change', function() {
 convertBtn.addEventListener('click', function() {
     if (images.length > 0) {
         const pdf = new jsPDF('portrait', 'mm', 'a4');
-        images.forEach((image, index) => {
-            const img = new Image();
-            img.src = URL.createObjectURL(image);
-            img.onload = function() {
-                const imgWidth = pdf.internal.pageSize.getWidth();
-                const imgHeight = img.naturalHeight * imgWidth / img.naturalWidth;
 
-                if (imgHeight > pdf.internal.pageSize.getHeight()) {
-                    const imgHeightScaled = pdf.internal.pageSize.getHeight();
-                    const imgWidthScaled = imgHeightScaled * img.naturalWidth / img.naturalHeight;
-                    pdf.addImage(img, 'PNG', (imgWidth - imgWidthScaled) / 2, 0, imgWidthScaled, imgHeightScaled);
-                } else {
-                    pdf.addImage(img, 'PNG', 0, (pdf.internal.pageSize.getHeight() - imgHeight) / 2, imgWidth, imgHeight);
-                }
+        const imagePromises = images.map((image, index) => {
+            return new Promise((resolve) => {
+                const img = new Image();
+                img.src = URL.createObjectURL(image);
+                img.onload = function() {
+                    const imgWidth = pdf.internal.pageSize.getWidth();
+                    const imgHeight = img.naturalHeight * imgWidth / img.naturalWidth;
 
-                if (index < images.length - 1) {
-                    pdf.addPage();
-                } else {
-                    pdf.save('download.pdf');
+                    if (imgHeight > pdf.internal.pageSize.getHeight()) {
+                        const imgHeightScaled = pdf.internal.pageSize.getHeight();
+                        const imgWidthScaled = imgHeightScaled * img.naturalWidth / img.naturalHeight;
+                        pdf.addImage(img, 'PNG', (imgWidth - imgWidthScaled) / 2, 0, imgWidthScaled, imgHeightScaled);
+                    } else {
+                        pdf.addImage(img, 'PNG', 0, (pdf.internal.pageSize.getHeight() - imgHeight) / 2, imgWidth, imgHeight);
+                    }
+
+                    if (index < images.length - 1) {
+                        pdf.addPage();
+                    }
+
+                    resolve();
                 }
-            }
+            });
+        });
+
+        Promise.all(imagePromises).then(() => {
+            pdf.save('download.pdf');
         });
     }
 });
